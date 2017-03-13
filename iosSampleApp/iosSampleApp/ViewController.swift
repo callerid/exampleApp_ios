8e418b19c8ed4db7f90656e4abdb3f0e0badb76d
Sam <spenland@callerid.com>
2017-03-13 16:59:07 -0400

    v.1.0.3 - began work on udp server
    - server on port 3520

--- iosSampleApp/iosSampleApp/ViewController.swift
+++ iosSampleApp/iosSampleApp/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CocoaAsyncSocket
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GCDAsyncUdpSocketDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    fileprivate var _socket: GCDAsyncUdpSocket?
+    fileprivate var socket: GCDAsyncUdpSocket? {
+        get {
+            if _socket == nil {
+                _socket = getNewSocket()
+            }
+            return _socket
+        }
+        set {
+            if _socket != nil {
+                _socket?.close()
+            }
+            _socket = newValue
+        }
+    }
+    
+    fileprivate func getNewSocket() -> GCDAsyncUdpSocket? {
+        
+        // set port to CallerID.com port --> 3520
+        let port = UInt16(3520)
+        
+        // Bind to CallerID.com port (3520)
+        let sock = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
+        do {
+            
+            try sock.bind(toPort: port)
+            try sock.enableBroadcast(true)
+            
+        } catch _ as NSError {
+            
+            return nil
+            
+        }
+        return sock
+    }
+    
+    @IBAction func startServer(_ sender: AnyObject) {
+        
+        do {
+            try socket?.beginReceiving()
+        } catch _ as NSError {
+            
+            return
+            
+        }
+        
+    }
+    
+    @IBAction func stopServer(_ sender: AnyObject) {
+        if socket != nil {
+            socket?.pauseReceiving()
+        }
+        
+    }
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        
+        if let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+            
+            // handle recieved data
+            
+            
+            
+        }
+        
+        
+        
+    }
 
 }
 
