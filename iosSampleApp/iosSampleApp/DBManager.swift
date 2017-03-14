4002672c84924eac705f29f1814adc5606b78218
Sam <spenland@callerid.com>
2017-03-14 17:00:01 -0400

    EOFC - 3/14/17
    End of day commit

---
+++ iosSampleApp/iosSampleApp/DBManager.swift
@@ -0,0 +1,18 @@
+//
+//  DBManager.swift
+//  iosSampleApp
+//
+//  Created by mac on 3/14/17.
+//  CallerID.com
+//
+
+import UIKit
+
+class DBManager: NSObject {
+
+    // Make singleton
+    static let shared: DBManager = DBManager()
+    
+    
+    
+}
